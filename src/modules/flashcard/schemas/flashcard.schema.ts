1ecf5d7d86ba37563fe7bd4c5ab52b5656aacbe5
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-22 21:35:46 +0700

    refactor: validate schema

--- src/modules/flashcard/schemas/flashcard.schema.ts
+++ src/modules/flashcard/schemas/flashcard.schema.ts
@@ -2,14 +2,6 @@ import z from 'zod';
 
 export const FlashcardSchema = z.object({
   id: z.number().optional().nullable(),
-  term: z
-    .string()
-    .trim()
-    .min(1, { message: 'term text is required' })
-    .max(200, { message: 'term text must be less than 200 characters' }),
-  definition: z
-    .string()
-    .trim()
-    .min(1, { message: 'Back text is required' })
-    .max(200, { message: 'Back text must be less than 200 characters' }),
+  term: z.string().trim().max(200, { message: 'term text must be less than 200 characters' }),
+  definition: z.string().trim().max(200, { message: 'Back text must be less than 200 characters' }),
 });
