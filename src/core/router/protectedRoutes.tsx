c04403f7de5b49b6d73ca682796b77c436a8ee15
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-24 23:43:05 +0700

    feat: add loading component

--- src/core/router/protectedRoutes.tsx
+++ src/core/router/protectedRoutes.tsx
@@ -1,3 +1,4 @@
+import Loading from '@/components/ui/loading';
 import { useAuth } from '@/shared/hooks/useAuth';
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
@@ -5,7 +6,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoutes = () => {
   const { isLoggedIn, isLoading } = useAuth();
 
-  if (isLoading) return <span>Loading...</span>;
+  if (isLoading) return <Loading />;
   if (!isLoggedIn) return <Navigate to="/login" replace />;
   return <Outlet />;
 };
